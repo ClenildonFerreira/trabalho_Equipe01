c3276fdc38a822c510de42b4e705ed1d83d0b574
Clenildon Ferreira <clenildonferreira34@gmail.com>
2021-02-23 23:22:24 -0300

    inicio do back-end

---
+++ springboot2-jpa-crud-usuario/src/main/java/net/fadam/springboot2/springboot2jpacrudusuario/Springboot2JpaCrudUsuarioApplication.java
@@ -0,0 +1,13 @@
+package net.fadam.springboot2.springboot2jpacrudusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2JpaCrudUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2JpaCrudUsuarioApplication.class, args);
+	}
+
+}
---
+++ springboot2-jpa-crud-usuario/src/test/java/net/fadam/springboot2/springboot2jpacrudusuario/Springboot2JpaCrudUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fadam.springboot2.springboot2jpacrudusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2JpaCrudUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
